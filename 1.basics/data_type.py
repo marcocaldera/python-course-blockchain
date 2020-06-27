896a44a450be2e250acc0de0b0a8a833c2d20959
Marco Caldera <marco.caldera@icloud.com>
2020-06-27 11:39:20 +0200

    Init

---
+++ 1.basics/data_type.py
@@ -0,0 +1,8 @@
+name = '10'
+
+# Per trasformare in: int() float() string()
+print(type(name))
+print(type(int(name)))
+
+long_number = 1_000_000 # Scrivere un numero con gli _ per migliorare la leggibilità
+print(long_number)
---
+++ README.md
@@ -0,0 +1,2 @@
+Per runnare un programma:
+`python3 file_name.py`
